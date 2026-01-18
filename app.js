6bb946a14db3b418c38f224e99b66fd8a393f8cb
Pampapathi K <pampapathi.k@dxc.com>
2026-01-18 17:38:57 +0530

    Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -52,22 +52,25 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        return  req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             // strore the data into our file
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            fs.writeFileSync('hello.txt', 'DUMMY');
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+           // fs.writeFileSync('message.txt', message);// it will block the next line until this line is executed
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+            // fs.writeFileSync('hello.txt', 'DUMMY');
+
         });
 
         res.setHeader('Content-Type', 'text/html');
-        res.write( '<head><title>My First Page</title></head>');
-        res.write('<body><h1>Hello from my Node.js Server!</h1></body>'); 
-        res.write( '<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        res.write('<html>');
         res.end();
     }
     console.log('Request made', res);// it will print in the terminal
