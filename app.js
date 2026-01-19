4b066a872a617b9dd315e3099940f88b8dd5170b
Pampapathi K <pampapathi.k@dxc.com>
2026-01-19 11:31:05 +0530

    debug

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const http = require('http');// it will alwasy look for the global modules
 const path = require('path');
 const os = require('os');
 const https = require('https');// 
+const routes = require('./routes');
 
 fs.writeFileSync('hello.txt', 'Hello, World! pampa', 'utf8');
 
@@ -22,7 +23,7 @@ fs.writeFileSync('hello.txt', 'Hello, World! pampa', 'utf8');
 
 /* http.createServer(rqListener); */
 
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
     //console.log(req.url, req.method, req.headers);
     // set header content type
     // console.log(req.url,req.method,req.headers);
@@ -32,7 +33,10 @@ const server = http.createServer((req, res) => {
     // const method = req.method;
 
     console.log('Request made', res);// it will print in the terminal
-});
+}); */
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 server.listen(3000);// port number
 
 /* stream and buffer the data data coming the stream of incoming requests and fully parsed the data */
